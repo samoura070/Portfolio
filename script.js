5c7018aa863b4bde076a930e8cad587af199a3a9
samoura070 <samiraabrar070@gmail.com>
2025-04-02 20:55:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+  const menu = document.querySelector(".mobile-links");
+  const icons = document.querySelector(".icons");
+  menu.classList.toggle("open");
+  icons.classList.toggle("open");
+}
